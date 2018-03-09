80167c52d7629f8653430aff20a5ae9a4c1c3e6b
camilo <cagudelovillegas@gmail.com>
2018-03-09 14:09:29 -0500

    Agregando funciones de llamada

--- client.py
+++ client.py
@@ -4,13 +4,65 @@ import pyaudio
 import wave
 
 
-
+WIDTH = 2
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "g.wav"
+
+
+def wire():
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        stream.write(data, CHUNK)
+
+    print("* done")
+
+    return data
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+def wireCallback():
+    p = pyaudio.PyAudio()
+
+    def callback(in_data, frame_count, time_info, status):
+        return (in_data, pyaudio.paContinue)
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    stream_callback=callback)
+
+    stream.start_stream()
+
+    while stream.is_active():
+        time.sleep(0.1)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
 
 
 def reproducir(frames):

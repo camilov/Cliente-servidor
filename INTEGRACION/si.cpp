612c1929ff6e4502f40cdfc1361e07f7af444613
camilo <cagudelovillegas@gmail.com>
2018-03-29 15:50:06 -0500

    borrando mas basura

--- INTEGRACION/si.cpp
+++ INTEGRACION/si.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include <omp.h>
 using namespace std;
 
 float a,b,n,h,suma,x,resultado;
@@ -13,18 +14,18 @@ int main()
 	//cout << "Ingrese funcion a evaluar: "; cin >> f;
 	h = (b-a)/n;
 	suma = 0.0;
+	#pragma omp parallel for
+		for(int i =1 ;i<n;i++){
+			x = a + i*h;
 
-	for(int i =1 ;i<n;i++){
-		x = a + i*h;
+			if(i %2 == 0){
+				suma = suma + 2 * sin(x);
 
-		if(i %2 == 0){
-			suma = suma + 2 * sin(x);
+			}else{
+				suma = suma + 4 * sin(x);
 
-		}else{
-			suma = suma + 4 * sin(x);
-
+			}
 		}
-	}
 	suma = suma + sin(a) +sin(b);
 
 	resultado = suma * (h/3);
